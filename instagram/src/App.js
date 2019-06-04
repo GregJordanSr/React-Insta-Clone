b74920dc892da1e497caa11dc74de73a9a4bf20d
Greg Jordan <gregljordan@gmail.com>
2019-06-04 18:12:44 -0400

    Going through and redo all code to understand.

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,22 +1,45 @@
-import React, {Component} from 'react';
-import dummyData from './dummyData';
-import PostContainer from './components/PostContainer/PostContainer';
-
+import React, { Component } from 'react';
+import './App.css';
+import dummyData from './dummy-data';
+import PostsContainer from './components/PostsContainer/PostsContainer';
+import SearchBar from './components/SearchBar/SearchBarContainer';
 
 class App extends Component {
-  render () {
-    return (
-      <div>{dummyData.map((user) => {
-        return <PostContainer user={user}/>
-      })}
-      </div>
-    )
-
+  constructor() {
+    super();
+    this.state = {
+      posts: [],
+      filteredPosts: []
+    };
   }
-   
+  componentDidMount() {
+    this.setState({ posts: dummyData });
+  }
+  searchPostsHandler = e => {
+    const posts = this.state.posts.filter(post =>{
+      if (post.username.includes(e.target.value)) {
+        return post;
+      }
+    });
+    this.setState({ filteredPosts: posts });
+  };
+  render() {
+    return (
+      <div className="App">
+        <SearchBar
+          searchTerm={this.state.searchTerm}
+          searchPosts={this.searchPostsHandler}
+        />
+        <PostsContainer
+          posts={
+            this.state.filteredPosts.length > 0
+              ? this.state.filteredPosts
+              : this.state.posts
+          }
+        />
+      </div>
+    );
+  }
 }
-
-
-
 
 export default App;
