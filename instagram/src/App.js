015a5c39fbbf0a722c4d649bd03ec5dddc3fd114
Greg Jordan <gregljordan@gmail.com>
2019-06-06 01:21:18 -0400

    setting state and CDM

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -2,14 +2,29 @@ import React from 'react';
 import './App.css';
 import SearchBar from './Components/SearchBar/SearchBar';
 import PostContainer from './Components/PostContainer/PostContainer';
+import dummyData from './dummy-data';
 
-function App() {
-  return (
-    <div className="App">
-      <SearchBar />
-      <PostContainer />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: []
+    }
+  }
+  componentDidMount() {
+    this.setState({data: dummyData})
+  }
+  
+  render () {
+    return (
+        <div className="App">
+          <SearchBar />
+          <PostContainer />
+        </div>
+    );
+
+  }
+  
 }
 
 export default App;
