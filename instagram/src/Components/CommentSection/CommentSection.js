aaa94e663a05390bc7e46a44cb8eafedb04d8d0c
Greg Jordan <gregljordan@gmail.com>
2019-06-06 00:42:27 -0400

    made skeleton of each component

---
+++ instagram/src/Components/CommentSection/CommentSection.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+ class CommentSection extends React.Component {
+    render() {
+        return (
+            <div>
+                CommentSection
+            </div>
+        )
+    }
+}
+
+export default CommentSection;
---
+++ instagram/src/Components/PostContainer/PostContainer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import CommentSection from '../CommentSection/CommentSection';
+
+class PostContainer extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <CommentSection />
+            </div>
+        )
+    }
+}
+
+export default PostContainer;
